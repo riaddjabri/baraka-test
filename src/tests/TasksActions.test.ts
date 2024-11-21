6be97a203610b0f631e1669fd01889dc3cc2903c
riaddjabri <djabri.riad@gmail.com>
2024-11-22 00:10:20 +0100

    - Added toggle test

--- src/tests/TasksActions.test.ts
+++ src/tests/TasksActions.test.ts
@@ -1,6 +1,6 @@
-import store, { addTask, RootState, AppDispatch } from '../store/TasksSlice';
+import store, {addTask, RootState, AppDispatch, toggleTask} from '../store/TasksSlice';
 
-describe('Redux Store', () => {
+describe('Items Actions test', () => {
 
     it('should have the correct initial state', () => {
         const state: RootState = store.getState();
@@ -19,5 +19,17 @@ describe('Redux Store', () => {
         expect(state.tasks.tasks.length).toBe(2);
         expect(state.tasks.tasks[0].task).toBe('Task 1');
         expect(state.tasks.tasks[1].task).toBe('Task 2');
+    });
+
+    it('should toggle a task correctly', () => {
+        const dispatch: AppDispatch = store.dispatch;
+        dispatch(addTask({ task: 'Task 1' }));
+        let state: RootState = store.getState();
+        const taskId = state.tasks.tasks[0].id;
+        expect(state.tasks.tasks[0].completed).toBe(false);
+        dispatch(toggleTask(taskId));
+        // reset state after modification
+        state = store.getState();
+        expect(state.tasks.tasks[0].completed).toBe(true);
     });
 });
